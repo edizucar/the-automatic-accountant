0abf8333d17fcccb3ae72d534fec5fbd9f08069d
Ediz Ucar <elu21@cam.ac.uk>
2022-02-08 12:26:24 +0000

    Added a bit to find the contents page

--- data-mining/TestingMiner/testing.py
+++ data-mining/TestingMiner/testing.py
@@ -1,4 +1,4 @@
-from pdfminer.high_level import extract_text
+from pdfminer.high_level import *
 import os
 from sys import platform
 
@@ -11,5 +11,13 @@ pdf_folder = os.chdir(f"..{x}data-files") # cd into the folder with data
 
 pdfs = [f for f in os.listdir() if f.endswith(".pdf")]
 num_pdfs = len(pdfs)
-contents,directors,comprehensive_income,balance_sheet,equity = [extract_text(pdfs[0],page_numbers=[i]) for i in [1,2,9,10,11]]
-print(contents)
+
+num_pages = len(list(extract_pages(pdfs[0])))
+
+contents_page = None
+for i in range(num_pages):
+    s=extract_text(pdfs[0],page_numbers=[i])
+    if "contents" in s.lower():
+        contents_page = s
+        break
+
