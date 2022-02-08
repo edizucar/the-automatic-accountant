ff9fe754407fc748ac059d7267182ab4783e05c5
danielvlasits <dvlasits@gmail.com>
2022-02-08 12:03:09 +0000

    import *

--- data-mining/TestingMiner/testing.py
+++ data-mining/TestingMiner/testing.py
@@ -1,4 +1,5 @@
-from pdfminer.high_level import extract_text
+from pdfminer.high_level import *
+
 import os
 from sys import platform
 
@@ -11,5 +12,5 @@ pdf_folder = os.chdir(f"..{x}data-files") # cd into the folder with data
 
 pdfs = [f for f in os.listdir() if f.endswith(".pdf")]
 num_pdfs = len(pdfs)
-contents,directors,comprehensive_income,balance_sheet,equity = [extract_text(pdfs[0],page_numbers=[i]) for i in [1,2,9,10,11]]
+contents,directors,comprehensive_income,balance_sheet,equity = [extract_pages(pdfs[0],page_numbers=[i]) for i in [1,2,9,10,11]]
 print(contents)
