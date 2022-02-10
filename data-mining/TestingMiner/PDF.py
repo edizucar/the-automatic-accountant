a436422337c646dd2ed5d0936e5a96cd7fc587b2
Vissarion <vissarionchristodoulou@gmail.com>
2022-02-10 15:58:13 +0000

    Added skeleton for finding relevant pages and content

--- data-mining/TestingMiner/PDF.py
+++ data-mining/TestingMiner/PDF.py
@@ -8,6 +8,11 @@ class PDF:
         self.__pages = {}
         self.__num_pages = self.pdfreader.getNumPages()
         self.__content_index = None
+        self.__directors_index = None
+        self.__balance_sheet_index = None
+        self.__equity_changes_index = None
+        self.__comprehensive_income_index = None
+
 
     def get_num_pages(self):
         return self.__num_pages
@@ -22,7 +27,7 @@ class PDF:
             self.__pages[i] = self.pdfreader.extractText(i)
             return self.__pages[i]
     
-    def find_contents_page_index(self):
+    def find_contents_index(self):
         if self.__content_index != None:
             return self.__content_index
 
@@ -33,9 +38,36 @@ class PDF:
                 return self.__content_index
         raise Exception("Contents page not found.")
 
-    def get_contents_page_content(self):
-        return self.get_page(self.find_contents_page_index())
+    def get_contents_content(self):
+        return self.get_page(self.find_contents_index())
 
+    def find_directors_index(self):
+        if self.__directors_index != None:
+            return self.__directors_index
+
+    def get_directors_content(self):
+        return self.get_page(self.find_directors_index())
+
+    def find_balance_sheet_index(self):
+        if self.__balance_sheet_index != None:
+            return self.__balance_sheet_index
+    
+    def get_balance_sheet_content(self):
+        return self.get_page(self.find_balance_sheet_index())
+
+    def find_equity_changes_index(self):
+        if self.__equity_changes_index != None:
+            return self.__equity_changes_index
+
+    def get_equity_changes_content(self):
+        return self.get_page(self.find_equity_changes_index())
+
+    def find_comprehensive_income_index(self):
+        if self.__comprehensive_income_index != None:
+            return self.__comprehensive_income_index
+
+    def get_contents_page_content(self):
+        return self.get_page(self.find_comprehensive_income_index())
     
 
             
