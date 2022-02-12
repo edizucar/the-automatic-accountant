08d9ae1b1241a6b91957804b01c104e6bff84ec0
Ediz Ucar <elu21@cam.ac.uk>
2022-02-12 19:29:29 +0000

    Update ixbrl-testing

--- data-mining/TestingMiner/ixbrl-testing.py
+++ data-mining/TestingMiner/ixbrl-testing.py
@@ -1,5 +1,6 @@
-from tkinter import N
+
 from ixbrlparse import IXBRL
+import re
 
 root = ""
 #f = root + "/Bilitech Ltd.xhtml"
@@ -9,27 +10,43 @@ with open(f, encoding="utf8") as a:
     x = IXBRL(a)
 
         #x.contexts
+if False:
+    for c in x.numeric:
+        print(c.name)
+        if c.context.instant != None:
+            print("instant: ", c.context.instant)
+        if c.context.startdate != None:
+            print("startdate: ", c.context.startdate)
+        if c.context.enddate != None:
+            print("enddate: ", c.context.enddate)
+        print(c.value)
+        print()
 
-for c in x.numeric:
-    print(c.name)
-    if c.context.instant != None:
-        print("instant: ", c.context.instant)
-    if c.context.startdate != None:
-         print("startdate: ", c.context.startdate)
-    if c.context.enddate != None:
-        print("enddate: ", c.context.enddate)
-    print(c.value)
-    print()
+def matchAny(patterns, string):
+    for pattern in patterns:
+        if re.search(pattern,string):
+            return True
+    return False
 
 
 #getting director names
+important_role_patterns = ["Chairman","ChiefExecutive","Director[0-9]+"]
+important_people = []
 nonnumeric_names=set()
+director_pattern = "Director[0-9]+"
 for a in x.nonnumeric:
-    nonnumeric_names.add(a.name)
+    
     if a.name == "NameEntityOfficer":
-        print(a.value)
+        val = a.context.segments[0]["value"]
+        if matchAny(important_role_patterns,val):
+            important_people.append(a)
 
 
-for name in nonnumeric_names:
-    #print(name)
-    pass
+for director in list(important_people):
+    c = director.context
+    val = c.segments[0]["value"]
+    print(f"value:{director.value}, segments:{val}")
+    print()
+
+
+
