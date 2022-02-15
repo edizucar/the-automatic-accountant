694b43f4846afcdbcf314faef352524d00250604
Judah_Daniels <34425995+judah-daniels@users.noreply.github.com>
2022-02-15 11:38:33 +0000

    Create Window with Upload Accounts button

--- Interface/interface.py
+++ Interface/interface.py
@@ -1,9 +1,62 @@
-from PyQt5.QtWidgets import QApplication, QLabel
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QPushButton, QHBoxLayout
+from PyQt5.QtGui import QIcon
 
-app = QApplication([])
+class App(QWidget):
 
-label = QLabel('Hello World!')
+    def __init__(self):
+        super().__init__()
+        self.title = 'The Automatic Accountant'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.layout = QHBoxLayout()
+        self.setLayout(self.layout)
+        
+        #self.openFileNameDialog()
+        #self.openFileNamesDialog()
+        #self.saveFileDialog()
+        
+        self.uploadAccountsButton = QPushButton("Upload Accounts")
+        self.uploadAccountsButton.setCheckable(True)
+        self.uploadAccountsButton.clicked.connect(self.uploadAccounts)
+        
+        #self.setCentralWidget(self.uploadAccountsButton)
+        self.layout.addWidget(self.uploadAccountsButton)
+        
+        self.show()
+    
+    def uploadAccounts(self):
+        files = self.openFileNamesDialog()
+    
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        if fileName:
+            print(fileName)
+    
+    def openFileNamesDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
+        if files:
+            return files
+    
+    def saveFileDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
+        if fileName:
+            print(fileName)
 
-label.show()
-
-app.exec()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
