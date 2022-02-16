e62eaaa49dda2888072d581d8f4cb06de039ce51
Thomas <thomasyuan1@gmail.com>
2022-02-16 22:43:13 +0000

    rough analysis methods

--- data_analysis/analysis-testing.py
+++ data_analysis/analysis-testing.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import os
+import matplotlib.pyplot as plt
 
 sys.path.append('.')
 sys.path.append('../data_analysis')
@@ -19,9 +20,34 @@ def readFromDataFolder(file_name):
     os.chdir(dir)
     return data
 
+#Find out percentage change of a specific index, eg. gross profit,
+#from data1 to data2
+def change(data1, data2, index):
+    index1 = data1[index]
+    index2 = data2[index]
+    return (index2 - index1) / (index1)
+
+#Just some examples of indices where we can look at the change over time
+def checkIfScam(data1, data2):
+    indices = []
+    for index in indices:
+        if abs(change(data1,data2, index)) > 0.2:
+            return True
+    return False    
+
+#Plot graphs of specific indices over time
+def plotOverTime(data, index):
+    lst = []
+    date = []
+    for d in data:
+        lst.append(d[index])
+        date.append(d["Date"])
+    plt.plot(lst, date)
+    plt.savefig('data.png')
+    
+
 def main():
     data = readFromDataFolder('data-files\\CE Statutory-Accounts-FY14-15.html')
     
-
 if __name__ == "__main__":
     main()
