d4c155e2749744f4997ff9871ccc17dfa7c766e7
danielvlasits <dvlasits@gmail.com>
2022-02-16 11:53:18 +0000

    Added code for a simple window

--- Interface/interface.py
+++ Interface/interface.py
@@ -1,2 +1,28 @@
 import PyQt5
 
+
+#!/usr/bin/python
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+
+
+def main():
+
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Automatic Accountant')
+    layout = QVBoxLayout()
+    layout.addWidget(QPushButton('Import Files'))
+    layout.addWidget(QPushButton('Bottom'))
+    w.setLayout(layout)
+    w.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
